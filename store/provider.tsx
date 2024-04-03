b63b6a932c9b92ddb764d60dbabb07c64cef9889
parkjise <monories79@gmail.com>
2024-04-03 14:40:36 +0900

    delete

--- store/provider.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { store } from "./store";
-import { Provider } from "react-redux";
-
-export function Providers({ children }: { children: any }) {
-  return <Provider store={store}>{children}</Provider>;
-}
